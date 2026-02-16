52d32b08b861bf12ba8a1ea4d66f1fc551d5fef5
Your Name <romaslinkevicius@gmail.com>
2026-02-16 14:30:36 +0200

    stooq

--- analysis.py
+++ analysis.py
@@ -1,61 +1,24 @@
-import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = yf.download("SPY", start="2015-01-01")
+url = "https://stooq.com/q/d/l/?s=spy.us&i=d"
 
-print(data.head())
+data = pd.read_csv(url)
+
+data["Date"] = pd.to_datetime(data["Date"])
+data = data.sort_values("Date")
+data.set_index("Date", inplace=True)
+
+print(data.tail())
 
 plt.figure()
 plt.plot(data["Close"])
-plt.title("SPY Closing Price")
+plt.title("SPY Closing Price (Stooq)")
 plt.show()
-# 50 dienų slenkantis vidurkis
-data["MA50"] = data["Close"].rolling(window=50).mean()
 
-# 200 dienų slenkantis vidurkis
-data["MA200"] = data["Close"].rolling(window=200).mean()
-
-plt.figure()
-plt.plot(data["Close"])
-plt.plot(data["MA50"])
-plt.plot(data["MA200"])
-plt.title("SPY su MA50 ir MA200")
-plt.show()
-import yfinance as yf
-
-data = yf.download("SPY", start="2015-01-01")
-
-print("Data shape:", data.shape)
-print(data.tail())
-import yfinance as yf
-
-data = yf.download("SPY", start="2015-01-01", auto_adjust=True)
-
-print("Data shape:", data.shape)
-print(data.tail())
-import yfinance as yf
-
-data = yf.download("SPY", period="10y", auto_adjust=True)
-
-data.to_csv("spy_data.csv")
-
-print("Saved locally.")
-import pandas as pd
-
-data = pd.read_csv("spy_data.csv", index_col=0, parse_dates=True)
-data = yf.download("SPY", period="5y", auto_adjust=True)
-print(data.tail())
-import yfinance as yf
-
-spy = yf.Ticker("SPY")
-data = spy.history(period="5y")
-
-print(data.tail())
-spy = yf.Ticker("^GSPC")
-data = spy.history(period="5y")
-
-print(data.tail())
-
-
+Open
+High
+Low
+Close
+Volume
 
