feb53938e7b3dc5a7345cb2b57d1ef9cf6500f6c
Your Name <romaslinkevicius@gmail.com>
2026-02-16 09:28:34 +0200

    Nauja_lentele_50_200

--- analysis.py
+++ analysis.py
@@ -10,5 +10,17 @@ plt.figure()
 plt.plot(data["Close"])
 plt.title("SPY Closing Price")
 plt.show()
+# 50 dienų slenkantis vidurkis
+data["MA50"] = data["Close"].rolling(window=50).mean()
+
+# 200 dienų slenkantis vidurkis
+data["MA200"] = data["Close"].rolling(window=200).mean()
+
+plt.figure()
+plt.plot(data["Close"])
+plt.plot(data["MA50"])
+plt.plot(data["MA200"])
+plt.title("SPY su MA50 ir MA200")
+plt.show()
 
 
